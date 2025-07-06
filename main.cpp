3e9c3161bb3e58b6c527e718ab9f84494abc052a
beast-machine-2 <krishivmanyam@gmail.com>
2025-07-06 12:29:12 -0700

    added event loops in main

--- main.cpp
+++ main.cpp
@@ -1,20 +1,92 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 
+#include "Car.h"
+#include <eigen-3.4.0/Eigen/Dense>
+
+#include "constants.h"
+
 int main(int argc, char* argv[]) {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
         return 1;
     }
 
-    SDL_Window* win = SDL_CreateWindow("Hello SDL2", 640, 480, 640, 480, SDL_WINDOW_SHOWN);
+    if (SDL_Init(SDL_INIT_JOYSTICK) != 0) {
+        std::cerr << "SLD_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+SDL_Window* win = SDL_CreateWindow("Car Game", Constants::SDL_WINDOW_X, Constants::SDL_WINDOW_Y, Constants::SDL_WINDOW_WIDTH, Constants::SDL_WINDOW_LENGTH, SDL_WINDOW_SHOWN);
     if (!win) {
         std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
         SDL_Quit();
         return 1;
     }
 
-    SDL_Delay(3000);
+    SDL_Renderer* renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+
+    if (!renderer) {
+        std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+    }
+
+    SDL_Joystick *joystick;
+
+    SDL_JoystickEventState(SDL_ENABLE);
+    joystick = SDL_JoystickOpen(0);
+
+    SDL_Event event;
+
+    Car car{Constants::CENTER_X, Constants::CENTER_Y, Constants::CAR_WIDTH, Constants::CAR_LENGTH};
+
+
+    Eigen::Vector2d a;
+
+
+    for (int i =0; i < 1000; i++) {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_KEYDOWN:
+                    switch (event.key.keysym.sym) {
+                        case SDLK_w:
+                                a = Constants::DOWN_VECTOR * 100;
+                                car.setAcceleration(a);
+                                break;
+                        case SDLK_s:
+                                a = Constants::UP_VECTOR * 100;
+                                car.setAcceleration(a);
+                                break;
+
+                        case SDLK_a:
+                            a = Constants::LEFT_VECTOR * 100;
+                            car.setAcceleration(a);
+                            break;
+                        case SDLK_d:
+                            a = Constants::RIGHT_VECTOR * 100;
+                            car.setAcceleration(a);
+                            break;
+                    }
+                    break;
+
+                case SDL_KEYUP:
+                    a = Constants::ZERO_VECTOR * 100;
+                    car.setAcceleration(a);
+                    break;
+
+
+
+
+                case SDL_QUIT:
+                    break;
+            }
+        }
+        car.eraseCar(renderer);
+        car.drawCar(renderer);
+        SDL_Delay(Constants::SDL_TIME_INTERVAL);
+        car.incrementTime(Constants::TIME_INTERVAL);
+    }
+
+
 
     SDL_DestroyWindow(win);
     SDL_Quit();
