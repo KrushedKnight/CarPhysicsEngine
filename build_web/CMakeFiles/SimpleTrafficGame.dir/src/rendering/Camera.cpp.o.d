594ce06a774d86f4f68da9376a3f89e1f2f8787a
Krishiv Manyam <krishivmanyam@gmail.com>
2026-01-07 12:55:35 -0800

    removed cached builds

--- build_web/CMakeFiles/SimpleTrafficGame.dir/src/rendering/Camera.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/SimpleTrafficGame.dir/src/rendering/Camera.cpp.o: \
-  /Users/krushedknight/Programming/BasicCarGame/src/rendering/Camera.cpp \
-  /Users/krushedknight/Programming/BasicCarGame/include/rendering/Camera.h
